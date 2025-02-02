53694d942b5fa90071d65c79696393277ade07f1
Marcos Vieira Jr. <marcosvieirajr@gmail.com>
2025-02-02 09:49:21 -0300

    feat: add Feat and Err methods

--- validator/error.go
+++ validator/error.go
@@ -1,5 +1,9 @@
 package validator
 
+import (
+	"fmt"
+)
+
 // Validation represents a field-level validation error.
 type Validation struct {
 	Field      string
@@ -12,6 +16,7 @@ type Error struct {
 	message     string
 	validations map[string]string
 	err         error
+	tmpField    string
 }
 
 // NewError initializes a new validation error with the given message and optional validations.
@@ -28,6 +33,18 @@ func NewError(message string, validations ...Validation) *Error {
 		message:     message,
 		validations: vs,
 	}
+}
+
+// Field sets the field name for the next validation error. This is useful for chaining validations.
+func (e *Error) Field(field string) *Error {
+	e.tmpField = field
+	return e
+}
+
+// Err sets the validation message for the current field. This is useful for chaining validations.
+func (e *Error) Err(validation string) *Error {
+	e.validations[e.tmpField] = validation
+	return e
 }
 
 // AddValidation adds a field-level validation error to the error instance.
@@ -58,10 +75,10 @@ func (e *Error) Validations() map[string]string {
 // Error returns a string representation of the validation error message.
 // If there is an underlying error, it will be included in the returned string.
 func (e *Error) Error() string {
-	if e.err != nil {
-		return e.err.Error()
+	if e.err == nil {
+		return e.message
 	}
-	return e.message
+	return fmt.Sprintf("%s: %s", e.message, e.err.Error())
 }
 
 // Wrap associates an underlying error with the validation error.
